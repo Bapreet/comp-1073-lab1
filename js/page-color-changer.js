d285fee879d39d21cb7d462df9c6e8df226c3e7b
Bapreet <75432662+Bapreet@users.noreply.github.com>
2024-02-07 12:19:03 -0500

    completed the lab

--- js/page-color-changer.js
+++ js/page-color-changer.js
@@ -1,25 +1,25 @@
+// Selecting HTML elements
 const red = document.querySelector("#red");
 const green = document.querySelector("#green");
 const blue = document.querySelector("#blue");
 const text = document.querySelector("html");
 
-red.addEventListener('change', getRedValue);
-green.addEventListener('change', getGreenValue);
-blue.addEventListener('change', getBlueValue);
+//Adding EventListeners
+red.addEventListener('change', getTheValue);
+green.addEventListener('change', getTheValue);
+blue.addEventListener('change', getTheValue);
 
-function getRedValue() {
+//Function to handle the change event
+function getTheValue() {
     let redValue = Number(red.value);
     console.log("red = " + redValue);
-}
-function getGreenValue() {
     let greenValue = Number(green.value);
     console.log("green = " + greenValue);
-}
-function getBlueValue() {
     let blueValue = Number(blue.value);
     console.log("blue = " + blueValue);
+
+    //setting the background color of HTML element to RBG values obtained
+    text.style.backgroundColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
 }
 
-function updatebodyColor() {
-    text.style.backgroundColor = 'rgb(${red'
-}
+
